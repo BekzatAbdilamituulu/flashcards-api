1bec0d8d536be4a5f9fcc1b726cc492d5d66d436
Bekzat <abdilamituulubekzat07@gmail.com>
2026-02-08 21:59:11 +0600

    Docker file is fixed

--- app/database.py
+++ app/database.py
@@ -5,12 +5,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-DB_PATH = BASE_DIR / "data" / "flashcards.db"
 
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////app/data/app.db")
+
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
+    DATABASE_URL,
     connect_args={"check_same_thread": False, "timeout": 5}
 )
 
